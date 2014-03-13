889babe0614c49f4e581e6dd0842ac6a680d4ad4
Elizabeth Barr <elizbarr@gmail.com>
2014-03-13 14:54:43 -0400

    Installed Parse SDK successfully

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+console.log('This is it! The project!');
+
+$(function(){
+  Parse.initialize("4C5G0XP4utK5POqJ4lcUE0ynbtQfxXE0wqVeCYaU", "KiiOSKGtBK3nQzQqtuyaPOSK6g189yGvLLN3fx4J");
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
+
+})
