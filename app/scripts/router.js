5fae8cc3fc9aa0616740f49b7c27273fa4a96f82
Elizabeth Barr <elizbarr@gmail.com>
2014-03-17 21:33:22 -0400

    router.js

---
+++ app/scripts/router.js
@@ -0,0 +1,62 @@
+var Router = Backbone.Router.extend({
+	routes: {
+		'': 'home',
+		'about': 'aboutPage',
+		'getHelp': 'getHelpPage',
+		'helpOut': 'helpOutPage',
+		'helpOut/:sort': 'sortEvents' //this will need to be fixed
+	},
+
+	initialize: function() {
+		this.events = new EventsCollection();
+		this.comments = new CommentsCollection();
+	},
+
+	home: function() {
+		$('body').empty();
+
+		new HeaderView();
+		new HomeView();
+
+
+		var query = new Parse.Query(EventClass);
+		query.limit(3);
+		query.ascending('eventDate');
+		query.find({
+			success: function(events){
+				for(var i=0; i < events.length; i++){
+					new ThumbnailView({model: events[i]});
+				}
+				new FooterView();
+			},
+			error: function(){
+				console.log('not so fast!');
+			}
+		});
+
+	},
+
+	aboutPage: function() {
+		$('body').empty();
+		new HeaderView();
+		new AboutView();
+		new FooterView();
+	},
+
+	getHelpPage: function() {
+		$('body').empty();
+		new HeaderView();
+		new GetHelpView();
+		new FooterView();
+	},
+
+	helpOutPage: function(sort) {
+		$('body').empty();
+		new HeaderView();
+		new HelpOutView();
+		new FooterView();
+	}
+
+
+
+});
